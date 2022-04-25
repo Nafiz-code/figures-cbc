007923873ddc77d2e719f2c70782c34b84b9bb97
Nafiz ABDOUL CARIME <n.abdoulcarime@epsve.fr>
2022-04-25 19:01:13 +0200

    modif de la figure

--- loc.r
+++ loc.r
@@ -1,16 +1,29 @@
 library(readxl)
-install.packages("kableExtra")
+install.packages("read_exel")
 library(kableExtra)
+library(readxl)
 library(tidyverse)
 
-localisation <- read_excel("localisation.xlsx")
-colnames(localisation) %>% str_squish()
+
+sophie <- read_excel("sophie.xlsx")
 
 
+rec = "Recurrence rate<br>by location (%)"
 
-localisation %>% kbl(align = 'lccccc',escape = F) %>% 
+byloc= c("10.8 %","7.4 %","9.1 %","20 %","9.1 %","12.5 %","4.5 %","8.8 %","3.7 %","9.5 %","1.3 %")
+
+sophie[rec] = byloc
+
+colnames(sophie) %>% str_squish()
+
+sophie = sophie %>% select(Location,`Primary BCC <br>(n=766) n,(%)`,`Recurrent BCC <br>(n=37) n,(%)`,rec,`p value*`)
+
+
+sophie %>% kbl(align = 'lccccc',escape = F) %>% 
   row_spec(0,bold = T) %>%
-  row_spec(21,bold = T) %>% 
   kable_classic(full_width = F) %>% 
-  footnote(symbol = "BCC histological subtypes") %>% 
+  footnote(symbol = "p value given for Fisher test with 'Trunk + Limbs' as reference value") %>% 
   save_kable("locccc.html")
+
+
+write.csv(sophie,'cbc')
